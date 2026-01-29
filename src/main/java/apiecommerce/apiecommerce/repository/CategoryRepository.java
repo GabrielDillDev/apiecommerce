7ab42af703124ac88021e04c039f1a70a0dedabb
GabrielDillDev <gabrieldill.dev@gmail.com>
2026-01-29 10:49:12 -0300

    Implementação das interfaces Repository da aplicação (User, Product, Category) (#3)

---
+++ src/main/java/apiecommerce/apiecommerce/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package apiecommerce.apiecommerce.repository;
+
+import apiecommerce.apiecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/apiecommerce/apiecommerce/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package apiecommerce.apiecommerce.repository;
+
+import apiecommerce.apiecommerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
